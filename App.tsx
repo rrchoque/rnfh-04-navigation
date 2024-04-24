f9d5a98a4fc13d65603d3c04fe4638286b9d6a9f
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-04-24 00:18:19 -0400

    Se instaló y configuró react-navigation, y los screems y directorios requeridos

---
+++ App.tsx
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import { Text } from 'react-native';
+
+function App(): React.JSX.Element {
+    return (
+      <NavigationContainer>
+        <Text>App.tsx</Text>
+      </NavigationContainer>
+    )
+}
+
+export default App;
