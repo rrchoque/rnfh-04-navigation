7c3227e3998d162d125adfd8125f8c0225b67e2f
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-04-25 09:29:43 -0400

    Se instaló y configuró el Drawer de react-navigation

--- App.tsx
+++ App.tsx
@@ -2,13 +2,13 @@ import 'react-native-gesture-handler';
 
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
-import { Text } from 'react-native';
-import { StackNavigator } from './src/presentation/routes/StackNavigator';
+//import { StackNavigator } from './src/presentation/routes/StackNavigator';
+import { SideMenuNavigator } from './src/presentation/routes/SideMenuNavigator';
 
 function App(): React.JSX.Element {
     return (
       <NavigationContainer>
-        <StackNavigator />
+        <SideMenuNavigator />
       </NavigationContainer>
     )
 }
