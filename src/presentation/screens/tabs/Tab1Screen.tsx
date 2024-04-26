fdadd51c9f9365de9db6a7797c868d479e8eb680
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-04-26 08:06:34 -0400

    Se agregó el botón del menú al tab 1

--- src/presentation/screens/tabs/Tab1Screen.tsx
+++ src/presentation/screens/tabs/Tab1Screen.tsx
@@ -1,6 +1,23 @@
-import { Text, View } from 'react-native';
+import { DrawerActions, useNavigation } from '@react-navigation/native';
+import { useEffect } from 'react';
+import { Pressable, Text, View } from 'react-native';
 
 export const Tab1Screen = () => {
+
+
+  const navigation = useNavigation();
+
+
+  useEffect( () => {
+    navigation.setOptions( {
+      headerLeft: () => (
+        <Pressable onPress={ () => navigation.dispatch( DrawerActions.toggleDrawer ) }>
+          <Text>Menú</Text>
+        </Pressable>
+      )
+    } );
+  }, [] );
+
   return (
     <View>
       <Text>Tab1Screen</Text>
